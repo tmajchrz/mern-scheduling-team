2c5c46a603824f49c0d1b7374adb3bd122eb6d7a
Jonathan Alvarez <jonathanryanalvarez699@gmail.com>
2022-04-26 14:35:15 -0400

    All tasks for SlotItem have been completed and I have also left a comment with instructions in case the dates for the startAt and endAt are still needed

--- frontend/src/components/SlotItem.jsx
+++ frontend/src/components/SlotItem.jsx
@@ -1,33 +1,66 @@
 import { useSelector } from 'react-redux'
 
 function SlotItem({ slot }) {
-  const { users } = useSelector(
-    (state) => state.users
-  )
-  const user = JSON.parse(localStorage.getItem('user'));
+  const { users } = useSelector((state) => state.users)
+  const user = JSON.parse(localStorage.getItem('user'))
 
   // Display empty if missing needed elements
   if (!user) {
-    return (<div></div>)
-  } 
+    return <div></div>
+  }
   if (!users) {
-    return (<div></div>)
-  } 
-
+    return <div></div>
+  }
 
   // TO DO -- Have it print just the start time as in 10:30 am followed
   // by a hyphen (-) followed by an end time followed by the volunteer(s).
   return (
-    <div className='slot'>
+    <div className="slot">
       <div>
         {/* {console.log(user._id)} */}
-        {new Date(slot.startAt).toLocaleString('en-US')} - 
+        {new Date(slot.startAt).toLocaleString('en-US', {
+          // If the dates are still needed then remove the comment syntax from the dateStyle below
+
+          // dateStyle: 'short',
+          timeStyle: 'short',
+        })}{' '}
+        -{' '}
+        {new Date(slot.endAt).toLocaleString('en-US', {
+          // If the dates are still needed then remove the comment syntax from the dateStyle below
+
+          // dateStyle: 'short',
+          timeStyle: 'short',
+        })}
       </div>
       <div>
-        {users.map(u => 
-          (slot.vol1 === u._id) ? 
-          <span key={slot.vol1}>{u.fname + ' ' + u.lname.charAt(0)}</span> : '' )}
-      </div> 
+        {users.map((u) =>
+          slot.vol1 === u._id && u.level === 'faculty' ? (
+            <span key={slot.vol1}>
+              <strong>{u.fname + ' ' + u.lname.charAt(0)}</strong>
+            </span>
+          ) : slot.vol1 === u._id ? (
+            <span key={slot.vol1}>
+              {', '} {u.fname + ' ' + u.lname.charAt(0)}
+            </span>
+          ) : (
+            ''
+          )
+        )}
+
+        {users.map((u) =>
+          slot.vol2 === u._id && u.level === 'faculty' ? (
+            <span key={slot.vol2}>
+              {', '} <strong>{u.fname + ' ' + u.lname.charAt(0)}</strong>
+            </span>
+          ) : slot.vol2 === u._id ? (
+            <span key={slot.vol2}>
+              {', '} {u.fname + ' ' + u.lname.charAt(0)}
+            </span>
+          ) : (
+            ''
+          )
+        )}
+      </div>
     </div>
   )
 }
